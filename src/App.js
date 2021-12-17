1ec0a624d56a546cf2599f0f91f34c3c4036cb75
Borna Vukusic <bornavukusic95@gmail.com>
2021-12-17 10:23:20 +0100

    touch ups

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ function App() {
   return (
     <div className="App">
       <div className="name">
-        <p> Borna Vukušić </p>{" "}
-      </div>{" "}
+        <p> Borna Vukušić </p>
+      </div>
       <div className="title">
-        <p> full-stack developer </p>{" "}
-      </div>{" "}
+        <p> full-stack developer </p>
+      </div>
       <div className="body">
         <p>
-          I 'm a 26 year-old Croatian full-stack developer with a recent focus
-          on front - end work.After graduating from {" "}
+          I am a 26 year-old Croatian full-stack developer with a recent focus
+          on front-end work. After graduating from{" "}
           <a
             className="App-link"
             href="https://www.fesb.unist.hr/"
@@ -20,15 +20,15 @@ function App() {
             rel="noreferrer"
           >
             FESB
-          </a>{" "}, I worked in
-          companies like{" "}
+          </a>
+          , I worked in companies like{" "}
           <a
             className="App-link"
             href="https://venio.hr/"
             target="_blank"
             rel="noreferrer"
           >
-            Venio Indicium{" "}
+            Venio Indicium
           </a>{" "}
           and{" "}
           <a
@@ -37,47 +37,46 @@ function App() {
             target="_blank"
             rel="noreferrer"
           >
-            Agilno{" "}
+            Agilno
           </a>{" "}
-          in Split, and{" "}
+          in Split, as well as{" "}
           <a
             className="App-link"
             href="https://www.sedamit.hr/"
             target="_blank"
             rel="noreferrer"
           >
-            Sedam IT{" "}
+            Sedam IT
           </a>{" "}
-          in Zagreb.{" "}
-        </p>{" "}
-      </div>{" "}
+          in Zagreb.
+        </p>
+      </div>
       <div className="work">
         <p>
           I am available for freelance work regarding React and React Native
-          projects.{" "}
-        </p>{" "}
-      </div>{" "}
+          projects.
+        </p>
+      </div>
+      <div className="footer" />
       <div className="contact">
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          .email{" "}
-        </a>{" "}
+        <a href="mailto:bvukusic.st@gmail.com" target="_blank" rel="noreferrer">
+          .email
+        </a>
         <a
-          href="https://reactjs.org"
-          target="_blank"
-          className="effect-underline"
-          rel="noreferrer"
-        >
-          .linkedin{" "}
-        </a>{" "}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/bvukusic/"
           target="_blank"
           rel="noreferrer"
         >
-          .github{" "}
-        </a>{" "}
-      </div>{" "}
+          .linkedin
+        </a>
+        <a
+          href="https://github.com/bvukusic"
+          target="_blank"
+          rel="noreferrer"
+        >
+          .github
+        </a>
+      </div>
     </div>
   );
 }
